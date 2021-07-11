e49e67837e18d8f2e21fe72ae6f64fbb006104bb
Henry Winters <hlwinters17@gmail.com>
2021-07-10 17:34:44 -0700

    Added function playRound to play a round of rock paper scissors against the computer

--- rps.js
+++ rps.js
@@ -6,17 +6,38 @@
 function computerPlay() { 
     let randomNumber = randomInt(3);
     if (randomNumber === 0) {
-        return "Rock";
+        return "rock";
     } else if (randomNumber === 1) {
-        return "Paper"; 
-    } else return "Scissors";
+        return "paper"; 
+    } else return "scissors";
 }
 
 function randomInt(max) { 
     return Math.floor(Math.random() * max);
+} 
+
+//create function that takes two parameters - one from user and one from computerPlay function
+//compare the parameters: if equal it's a tie, otherwise paper beats rock, rock beats scissors, scissors beats paper
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection.toLowerCase();
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "Congratulations, you win! Rock beats scissors.";
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "Ouch, you lose! Paper beats rock.";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "Congratulations, you win! Paper beats rock.";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "Ouch, you lose! Scissors beat paper.";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "Congratulations, you win! Scissors beat paper.";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "Ouch, you lose! Rock beats scissors.";
+    } else return "Hey, that's not one of the options! Please input rock, paper, or scissors."
 }
 
-computerPlay(); 
-
-
-
+const playerSelection = "rock";
+const computerSelection = computerPlay(); 
+console.log(playRound(playerSelection, computerSelection));
