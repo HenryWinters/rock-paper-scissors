c2ab941d988847e6e978388e24420ae9cabe2a3e
Henry Winters <hlwinters17@gmail.com>
2021-07-15 23:35:48 -0700

    Removed unecessary notes

--- rps.js
+++ rps.js
@@ -16,8 +16,6 @@ function randomInt(max) {
     return Math.floor(Math.random() * max);
 } 
 
-//create function that takes two parameters - one from user and one from computerPlay function
-//compare the parameters: if equal it's a tie, otherwise paper beats rock, rock beats scissors, scissors beats paper
 let tie = "It's a tie!"; 
 let rockWin = "Nice, you win! Rock beats scissors";
 let rockLose = "Ouch, you lose! Paper beats rock";
@@ -155,47 +153,3 @@ function selectPlayerIcon() {
     }
     playerSelectionDisplay.appendChild(playerSelectionDisplayIcon);
 };
-
-
-
-
-
-
-
-//play a five round game of rock paper scissors
-//create point tracker for the player and the computer
-//create round tracker to track what round we are in
-//if player wins, add 1 point to the player tracker 
-//if computer wins, add 1 point to the computer tracker
-//after 5 rounds, compare the numbers 
-//if player number is higher than computer number, declare player the winner 
-//else declare the computer winner
-//either way display point values for each
-/*
-function game() {
-    let round = 1; 
-    let playerPoints = 0; 
-    let computerPoints = 0; 
-    while (round <= 5) { 
-        console.log("Round " + round);
-        let playerSelection = prompt("What's your selection? Rock, paper, or scissors?");
-        let computerSelection = computerPlay();
-        let roundResult = playRound(playerSelection, computerSelection);
-        console.log(roundResult);
-        if (roundResult === "Nice, you win! Rock beats scissors." || roundResult === "Nice, you win! Paper beats rock." || roundResult === "Nice, you win! Scissors beat paper.") {
-            playerPoints += 1;
-        } else if (roundResult === "Ouch, you lose! Paper beats rock." || roundResult === "Ouch, you lose! Scissors beat paper." || roundResult === "Ouch, you lose! Rock beats scissors.") {
-            computerPoints += 1; 
-        } else { playerPoints += 0; round--  
-        }
-        round++ 
-        console.log("Player Points " + playerPoints);
-        console.log("Computer Points " + computerPoints);
-        if (round === 6) {
-            if (playerPoints > computerPoints) {
-                console.log("That's a wrap. You beat the computer. Congratulations!"); 
-            } else console.log("That's a wrap. You lost to the computer. Better luck next time!");
-        }
-    }
-}
-*/
